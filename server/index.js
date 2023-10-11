fc718132f18b3856a44e2c236df9687906d0e921
cristiantrilleras <cris_01lds@yahoo.es>
2023-10-10 22:08:51 -0500

    arregle server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import {Server as SocketServer} from 'socket.io';
 import http from "http";
-import cors from 'cors;'
+import cors from 'cors';
 import { PORT } from './config.js'
 
 const app = express();
@@ -26,3 +26,5 @@ io.on('connection', socket => {
 
 server.listen (PORT);
 console.log("Server started on port: "+PORT);
+
+
